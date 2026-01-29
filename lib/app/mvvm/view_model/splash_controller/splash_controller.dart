4c4ea3d91e011f6affbc3a74c3b75e6e5744acc0
Muhammad Rubaz Khan <rubazjamshaid@gmail.com>
2026-01-29 10:40:22 +0500

    code

--- lib/app/mvvm/view_model/splash_controller/splash_controller.dart
+++ lib/app/mvvm/view_model/splash_controller/splash_controller.dart
@@ -1,8 +1,9 @@
 import 'package:get/get.dart';
+import 'package:safewallet/app/config/app_routes.dart';
 
 class SplashController extends GetxController {
   void decideaAndNavigation() async {
-    // await Future.delayed(const Duration(seconds: 3));
-    // Get.offAllNamed(AppRoutes.onboardingView);
+    await Future.delayed(const Duration(seconds: 3));
+    Get.offAllNamed(AppRoutes.onboardingView);
   }
 }
